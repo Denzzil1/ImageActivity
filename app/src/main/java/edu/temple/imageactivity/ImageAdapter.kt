ac65d68c09f746a2fd9cd6154c50e67891c2f38a
Cory <denzzil123@gmail.com>
2022-02-24 08:54:33 -0500

    Added images, constraints and interactivity with the images.

--- app/src/main/java/edu/temple/imageactivity/ImageAdapter.kt
+++
@@ -1,4 +0,0 @@
-package edu.temple.imageactivity
-
-class ImageAdapter {
-}
--- app/src/main/java/edu/temple/imageactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/imageactivity/MainActivity.kt
@@ -1,11 +1,36 @@
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         package edu.temple.imageactivity
-
+package edu.temple.imageactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainAct : AppCompatActivity(), ImageAdapter.ImageOperations {
+    lateinit var foodName: TextView
+    lateinit var foodImg: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        foodName = findViewById(R.id.names)
+        foodImg = findViewById(R.id.food_img)
+        recyclerView.layoutManager =  GridLayoutManager(this, 4)//creates grid for recycler
+        //creating a list of images and names
+        val imgList = listOf<Int>(R.drawable.blueberries, R.drawable.burger, R.drawable.cake, R.drawable.frenchfries, R.drawable.grcheese, R.drawable.hotdog, R.drawable.mashed_potatos, R.drawable.pasta,
+            R.drawable.ribs, R.drawable.sandwich, R.drawable.soup, R.drawable.taco)//drawables for the images
+
+        val dogNames = listOf<String>("blueberries", "burger", "cake", "frenchfries", "grilled cheese", "hotdog", "mashed potatos",
+            "pasta", "ribs", "sandwich", "soup", "taco")
+        recyclerView.adapter = ImageAdapter(this, imgList, dogNames, this)
+//recyclerview setup and passes the images
+    }
+
+    override fun onImageClicked(name: String, img: Int) {
+        //when image is clicked in recyclerview set image and name
+        foodName.text = name;
+        foodImg.setImageResource(img)
     }
 }
